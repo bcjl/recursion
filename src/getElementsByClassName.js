c90da825b400d98d466af59bcdda8839bf8f3b09
Brian Liu <briancjliu@gmail.com>
2015-02-27 14:38:05 -0800

    getElementsByClassName done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  var matchedEle = [];
+  
+  for(var i = 0; i < $('.' + className).length; i++){
+  	matchedEle.push($('.' + className)[i]);
+  }
+
+  return matchedEle;
+
 };
