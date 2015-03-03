96e2cda15c96a2f0bcc9f1998ffcc8ea08add831
Brian Liu <briancjliu@gmail.com>
2015-03-03 00:57:09 -0800

    Stuck on processing key/value pairs of objects

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,49 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var output;
+  var insides = json.substring(1, json.length - 2);
+
+  var firstchar = json.substring(0,1);
+  var lastchar = json.substring(json.length - 2);
+
+  if(firstchar === "\"" && lastchar === "\""){
+    output = "" + insides;
+  }
+
+  else if(firstchar === "[" && lastchar === "]"){
+  	output = [];
+    var arrayInsides = insides.split(",");
+    if(json.length > 2){
+      for(var i = 0; i < arrayInsides.length; i++){
+        output.push(parseJSON(arrayInsides[i].trim()));
+      }
+    }
+  	// if(json.substring(json.length - 1) !== "]"){
+  	// 	throw new SyntaxError();
+  	// }
+  }
+
+  else if(firstchar === "{" && lastchar === "}"){
+  	output = {};
+    var propPairs = insides.split(",");
+    if (json.length > 2){
+      for(var i = 0; i < propPairs.length; i++){
+        var keyvalArr = propPairs[i].split(":");
+        var key = keyvalArr[0].trim();
+        var val = keyvalArr[1].trim();
+
+        output[key] = parseJSON(val);
+      }
+    }
+  	if(json.substring(json.length - 1) !== "}"){
+  		throw new SyntaxError();
+  	}
+  }
+
+  else{
+    output = eval(json);
+  }
+
+  return output;
 };
