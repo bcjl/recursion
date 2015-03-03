4371a592634947788e991c408fcb9d4283d2cfa1
Brian Liu <briancjliu@gmail.com>
2015-03-03 03:26:30 -0800

    Objects work up to keys with a comma inside, need to refactor code

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,10 +4,10 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   var output;
-  var insides = json.substring(1, json.length - 2);
+  var insides = json.substring(1, json.length - 1);
 
   var firstchar = json.substring(0,1);
-  var lastchar = json.substring(json.length - 2);
+  var lastchar = json.substring(json.length - 1);
 
   if(firstchar === "\"" && lastchar === "\""){
     output = "" + insides;
@@ -28,19 +28,28 @@ var parseJSON = function(json) {
 
   else if(firstchar === "{" && lastchar === "}"){
   	output = {};
+    // var startpos = insides.indexOf("\"");
+    // var endpos = insides.indexOf("\"", startpos + 1);
+
+    // var key = json.substring(startpos + 1, endpos);
+    // json = json.substring(endpos + 1);
+
     var propPairs = insides.split(",");
     if (json.length > 2){
       for(var i = 0; i < propPairs.length; i++){
         var keyvalArr = propPairs[i].split(":");
         var key = keyvalArr[0].trim();
+        key = key.substring(1, key.length - 1);
+        console.log(keyvalArr[0]);
+        console.log(key);
         var val = keyvalArr[1].trim();
 
         output[key] = parseJSON(val);
       }
     }
-  	if(json.substring(json.length - 1) !== "}"){
-  		throw new SyntaxError();
-  	}
+  	// if(json.substring(json.length - 1) !== "}"){
+  	// 	throw new SyntaxError();
+  	// }
   }
 
   else{
@@ -49,3 +58,13 @@ var parseJSON = function(json) {
 
   return output;
 };
+
+
+// var RemoveQuotes = function(string){
+//   var output = "";
+//   for (var i = 0; i < string.length; i++){
+//     if(){
+
+//     }
+//   }
+// }
